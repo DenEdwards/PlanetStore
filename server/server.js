a79e2f4dec9a4759be558a1313b7693063769d10
DenEdwards <denzelsedwards@gmail.com>
2020-08-19 18:59:53 -0400

    adding Access-Control-Allow-Origin

--- server/server.js
+++ server/server.js
@@ -18,12 +18,24 @@ app.use(cors());
 //allows server to parse objects as json objects
 app.use(express.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+    // Pass to next layer of middleware
+    next();
+ });
 
 //connect to mongodb atlas with our .env variable
 mongoose.connect(process.env.MONGODB_ATLAS, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
