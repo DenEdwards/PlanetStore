2ae394a2430cd7d3fd8be2bbadb8b90df1813a32
DenEdwards <denzelsedwards@gmail.com>
2020-08-20 13:55:44 -0400

    Changes for Admin page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,6 @@ function Navbar(){
         <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
         {/* Instead of anchor we use Link */}
             <Link to="/" className="navbar-brand"> <h1>PlanetStore</h1> </Link>
-            <div className="collpase navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
-                        <Link to="/add" className="nav-link">Add Item</Link>
-                    </li>
-                    <li className="navbar-item">
-                        <Link to="/admin" className="nav-link">Admin</Link>
-                    </li>
-                </ul>
-            </div>
         </nav>
     );
 }
--- src/components/routes/admin.route.js
+++ src/components/routes/admin.route.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { Link } from 'react-router-dom';
+
 // NEED TO FIGURE OUT PROBLEM WITH TOTAL 
 function Admin(){
 
@@ -28,7 +30,7 @@ function Admin(){
     
     return(
     <div>
-        <h1 style={{marginLeft:"30px"}}>Admin</h1>
+        <h1 style={{margin:"30px"}}>Admin</h1><button className="btn btn-success"><Link to="/add" className="nav-link">Add Item</Link></button>
         {state.orders.map((order, index) =>{
             return(
                 <div className="inventory" key={index}>
